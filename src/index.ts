7885821a8ba9388d5e2008f9e78a69616da45d40
Kevin Hutchins <possessedcow@gmail.com>
2024-12-28 22:43:08 -0800

    Add sortablejs, tweak row creation.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import { ZipEntry } from "@zip.js/zip.js";
 import ComicMerge, { ZipFile } from "./ComicMerge";
-
-const zip = require("@zip.js/zip.js");
+import Sortable from 'sortablejs';
 
 export const cbzMerge = new ComicMerge();
 
@@ -43,26 +43,28 @@ cbzMerge.addListener(() => refreshList());
 let fileList = document.getElementById("file-list") as HTMLDivElement;
 async function refreshList() {
     const newFileList = fileList.cloneNode() as HTMLDivElement;
-    for (let i = 0; i < cbzMerge.files.length; i++) {
-        const file = cbzMerge.files[i];
-        const entries = await file.waitForEntries();
-        const li = document.createElement("li");
-        const filenameContainer = document.createElement("span");
-        const filename = document.createElement("span");
-        filenameContainer.classList.add("filename-container");
-        li.appendChild(filenameContainer);
-        filename.classList.add("filename");
-        filename.dataset.entryIndex = '' + i;
-        filename.textContent = filename.title = `${file.file.name}`;
-        const fileCount = document.createElement("span");
-        fileCount.classList.add("filecount");
-        fileCount.textContent = `${file.entries.length}`;
-        filenameContainer.appendChild(filename);
-        filenameContainer.appendChild(fileCount);
-        newFileList.appendChild(li);
+    for (const file of cbzMerge.files) {
+        await file.waitForEntries();
+        newFileList.appendChild(addRow(file));
     };
     fileList.replaceWith(newFileList);
     fileList = newFileList;
+}
+
+function addRow(file: ZipFile): HTMLElement {
+    const li = document.createElement("li") as HTMLElement;
+    const filenameContainer = document.createElement("span");
+    const filename = document.createElement("span");
+    filenameContainer.classList.add("filename-container");
+    li.appendChild(filenameContainer);
+    filename.classList.add("filename");
+    filename.textContent = filename.title = `${file.file.name}`;
+    const fileCount = document.createElement("span");
+    fileCount.classList.add("filecount");
+    fileCount.textContent = `${file.entries.length}`;
+    filenameContainer.appendChild(filename);
+    filenameContainer.appendChild(fileCount);
+    return li;
 }
 
 const downloadButton = document.getElementById("download-button");
